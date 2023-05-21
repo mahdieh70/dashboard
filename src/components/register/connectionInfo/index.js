1445352e943be199b1ca1de9abdef2fa0de4afc1
mahdieh mahmoodi <mahdieh.mahmoodi@gmail.com>
2023-05-21 23:10:08 +0430

    register component updated

--- src/components/register/connectionInfo/index.js
+++ src/components/register/connectionInfo/index.js
@@ -3,7 +3,6 @@ import PinInput from "react-pin-input";
 import clipBoardTickIcon from "../../../assets/icons/clipboardtick.svg";
 import { TextInput } from "../../textInput";
 
-
 //icons
 import mobileIcon from "../../../assets/icons/mobile.svg";
 import messageText from "../../../assets/icons/messagetext1.svg";
@@ -21,7 +20,6 @@ const ConnectionInfo = ({ increaseStepHandler, decreaseStepHandler }) => {
   const [isSendingCode, setisSendingCode] = useState(false);
   const [pincode, setPincode] = useState("");
   const [isPincodeValid, setIspincodeValid] = useState(false);
- 
 
   const formik = useFormik({
     initialValues: {
@@ -42,8 +40,9 @@ const ConnectionInfo = ({ increaseStepHandler, decreaseStepHandler }) => {
         .required("لطفا ایمیل خود را وارد کنید."),
     }),
     onSubmit: () => {
-      console.log("submit");
-      increaseStepHandler();
+      if (isPincodeValid) {
+        increaseStepHandler();
+      }
     },
   });
 
@@ -65,117 +64,125 @@ const ConnectionInfo = ({ increaseStepHandler, decreaseStepHandler }) => {
 
   return (
     <div className="w-full h-full">
-        <div className="flex items-center flex-col h-[76vh] text-center px-[130px] border border-solid border-[#D6D6D6] border-t-0 border-r-0 border-l-0">
-          <div>
-            <p className="text-[#388AEA]">مرحله 2 از 3</p>
-            <p className="pt-2 mb-8 text-[#1e1e1e]">
-              لطفا اطلاعات خود را با دقت وارد نمائید
-            </p>
-          </div>
-          <div className="w-full flex justify-center items-center">
-            <form className="w-[580px]">
-              <div className="mb-4 relative">
-                <TextInput
-                  icon={<img src={mobileIcon} alt="mobileIcon" />}
-                  label={"شماره همراه"}
-                  placeholder={"09112564798"}
-                  type={"nubmer"}
-                  {...formik.getFieldProps("phoneNumber")}
-                />
-                {!isPincodeValid ? (
-                  <button
-                    disabled={!isPhoneNumberValid}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      setisSendingCode(true);
-                    }}
-                    className="absolute top-[33.33%] left-[20px] text-[#388AEA]"
-                  >
-                    ارسال کد
-                  </button>
-                ) : null}
-
-                {formik.errors.phoneNumber ? (
-                  <div className="text-rose-700 text-[14px]">
-                    {formik.errors.phoneNumber}
-                  </div>
-                ) : null}
-              </div>
-              {isSendingCode ? (
-                <>
-                  <div className="h-[60px] bg-[#E8F4FF] rounded-[50px] flex items-center">
-                    <div className="w-10 h-5 mr-5">
-                      <img src={clipBoardTickIcon} alt="clipBoardTickIcon" />
-                    </div>
-                    <p className="text-[14px]">
-                      کد تائید به شماره 09015671346 ارسال شده است. این کد تا
-                      02:00 دقیقه دیگر معتبر است
-                    </p>
-                  </div>
-                  <div className="flex justify-center items-center flex-col">
-                    <p className="mt-6 mb-[10px]">کد تائید</p>
-                    <PinInput
-                      length={4}
-                      type="numeric"
-                      inputMode="numeric"
-                      style={{ padding: "10px", direction: "ltr" }}
-                      inputStyle={{
-                        borderColor: "#D6D6D6",
-                        borderRadius: "16px",
-                      }}
-                      onChange={(value) => {
-                        setPincode(value);
-                      }}
-                      // onComplete={(value, index) => {}}
-                      autoSelect={true}
-                      // regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
-                    />
-
-                    <button
-                      onClick={(e) => {
-                        e.preventDefault();
-                        handleOnConfirmPincode();
-                      }}
-                      className="button mb-8 mt-2"
-                    >
-                      تائید شماره همراه
-                    </button>
-                  </div>
-                </>
+      <div className="flex items-center flex-col h-[76vh] text-center px-[130px] border border-solid border-[#D6D6D6] border-t-0 border-r-0 border-l-0">
+        <div>
+          <p className="text-[#388AEA]">مرحله 2 از 3</p>
+          <p className="pt-2 mb-8 text-[#1e1e1e]">
+            لطفا اطلاعات خود را با دقت وارد نمائید
+          </p>
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <form className="w-[580px]">
+            <div className="mb-4 relative">
+              <TextInput
+                icon={<img src={mobileIcon} alt="mobileIcon" />}
+                label={"شماره همراه"}
+                placeholder={"09112564798"}
+                type={"nubmer"}
+                {...formik.getFieldProps("phoneNumber")}
+              />
+              {!isPincodeValid ? (
+                <button
+                  disabled={!isPhoneNumberValid}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setisSendingCode(true);
+                  }}
+                  className="absolute top-[33.33%] left-[20px] text-[#388AEA]"
+                  style={{ color: isPhoneNumberValid ? "#388AEA" : "gray" }}
+                >
+                  ارسال کد
+                </button>
               ) : null}
 
-              <div className="mb-8">
-                <TextInput
-                  icon={<img src={messageText} alt="messageTextIcon" />}
-                  label={"ایمیل"}
-                  type="email"
-                  placeholder={"example@mail.com"}
-                  {...formik.getFieldProps("email")}
-                />
-                {formik.errors.email ? (
-                  <div className="text-rose-700 text-[14px]">
-                    {formik.errors.email}
+              {formik.errors.phoneNumber ? (
+                <div className="text-rose-700 text-[14px]">
+                  {formik.errors.phoneNumber}
+                </div>
+              ) : null}
+            </div>
+            {isSendingCode ? (
+              <>
+                <div className="h-[60px] bg-[#E8F4FF] rounded-[50px] flex items-center">
+                  <div className="w-10 h-5 mr-5">
+                    <img src={clipBoardTickIcon} alt="clipBoardTickIcon" />
                   </div>
-                ) : null}
-              </div>
-            </form>
-          </div>
-        </div>
-        <div className="flex flex-row-reverse justify-between items-center h-[11vh]">
-          <button onClick={handleSub} type="submit" className="deActiveButton">
-            مرحله بعد <BsArrowLeft />
-          </button>
+                  <p className="text-[14px]">
+                    کد تائید به شماره 09015671346 ارسال شده است. این کد تا 02:00
+                    دقیقه دیگر معتبر است
+                  </p>
+                </div>
+                <div className="flex justify-center items-center flex-col">
+                  <p className="mt-6 mb-[10px]">کد تائید</p>
+                  <PinInput
+                    length={4}
+                    type="numeric"
+                    inputMode="numeric"
+                    style={{ padding: "10px", direction: "ltr" }}
+                    inputStyle={{
+                      borderColor: "#D6D6D6",
+                      borderRadius: "16px",
+                    }}
+                    onChange={(value) => {
+                      setPincode(value);
+                    }}
+                    // onComplete={(value, index) => {}}
+                    autoSelect={true}
+                    // regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
+                  />
 
-          <a
-            onClick={decreaseStepHandler}
-            href="#"
-            className="text-[#388AEA] no-underline"
-          >
-            مرحله قبل
-          </a>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleOnConfirmPincode();
+                    }}
+                    className="button mb-8 mt-2"
+                  >
+                    تائید شماره همراه
+                  </button>
+                </div>
+              </>
+            ) : null}
+
+            <div className="mb-8">
+              <TextInput
+                icon={<img src={messageText} alt="messageTextIcon" />}
+                label={"ایمیل"}
+                type="email"
+                placeholder={"example@mail.com"}
+                {...formik.getFieldProps("email")}
+              />
+              {formik.errors.email ? (
+                <div className="text-rose-700 text-[14px]">
+                  {formik.errors.email}
+                </div>
+              ) : null}
+            </div>
+          </form>
         </div>
       </div>
-   
+      <div className="flex flex-row-reverse justify-between items-center h-[11vh]">
+        <button
+          onClick={handleSub}
+          type="submit"
+          className={
+            !formik.errors.phoneNumber && isPincodeValid && !formik.errors.email
+              ? "button"
+              : "deActiveButton"
+          }
+        >
+          مرحله بعد <BsArrowLeft />
+        </button>
+
+        <a
+          onClick={decreaseStepHandler}
+          href="#"
+          className="text-[#388AEA] no-underline"
+        >
+          مرحله قبل
+        </a>
+      </div>
+    </div>
   );
 };
 
--- src/components/register/locationInfo/index.js
+++ src/components/register/locationInfo/index.js
@@ -20,11 +20,10 @@ import { useFormik } from "formik";
 import { cities } from "../../../data/cities";
 import { provinces } from "../../../data/provinces";
 
-
 //yup
 import * as Yup from "yup";
 
-const LocationInfo = ({decreaseStepHandler}) => {
+const LocationInfo = ({ decreaseStepHandler }) => {
   const [showModal, setIsShowModal] = useState(false);
   // const [selectecProvince, setSelectedProvince] = useState("27");
   const navigate = useNavigate();
@@ -52,8 +51,8 @@ const LocationInfo = ({decreaseStepHandler}) => {
 
   const selectecProvince = formik.values.province;
 
-  const provinceRelatedCities = cities.filter(({ province }) => {
-    return String(province) === selectecProvince;
+  const provinceRelatedCities = cities.filter((item) => {
+    return (item.province).toString() === selectecProvince;
   });
 
   return (
@@ -62,120 +61,117 @@ const LocationInfo = ({decreaseStepHandler}) => {
         <ModalContent onClose={() => setIsShowModal((prev) => !prev)} />
       </Modal>
       <div className="w-full h-full">
-          <div className="flex items-center flex-col h-[76vh] text-center border border-solid border-[#D6D6D6] border-t-0 border-r-0 border-l-0">
-            <div>
-              <p className="text-[#388AEA]">مرحله 3 از 3</p>
-              <p className="pt-2 mb-8 text-[#1e1e1e]">
-                لطفا اطلاعات خود را با دقت وارد نمائید
-              </p>
-            </div>
-            <form></form>
-            <div className=" flex flex-col gap-[40px]">
-              <div className="relative w-[660px] gap-[16px] flex">
-                <div className="flex items-center w-[322px] h-[60px] relative rounded-[50px] border border-solid border-[#D6D6D6]">
-                  <div className="px-[15px] pr-[15px] border-l border-solid border-[#D6D6D6]">
-                    <img src={location} alt="location" />
-                  </div>
-                  <div className="relative w-full flex px-[10px]">
-                    <label
-                      htmlFor="province"
-                      className="absolute -top-[32px] -right-[16px] px-[10px] bg-white"
-                    >
-                      استان
-                    </label>
-                    <select
-                      {...formik.getFieldProps("province")}
-                      className="w-full"
-                    >
-                      {provinces.map((province) => (
-                        <option value={province.id} key={province.id}>
-                          {province.name}
-                        </option>
-                      ))}
-                    </select>
-                    {formik.errors.province ? (
-                      <div className="text-rose-700 text-[14px]">
-                        {formik.errors.province}
-                      </div>
-                    ) : null}
-                  </div>
+        <div className="flex items-center flex-col h-[76vh] text-center border border-solid border-[#D6D6D6] border-t-0 border-r-0 border-l-0">
+          <div>
+            <p className="text-[#388AEA]">مرحله 3 از 3</p>
+            <p className="pt-2 mb-8 text-[#1e1e1e]">
+              لطفا اطلاعات خود را با دقت وارد نمائید
+            </p>
+          </div>
+          <form></form>
+          <div className=" flex flex-col gap-[40px]">
+            <div className="relative w-[660px] gap-[16px] flex">
+              <div className="flex items-center w-[322px] h-[60px] relative rounded-[50px] border border-solid border-[#D6D6D6]">
+                <div className="px-[15px] pr-[15px] border-l border-solid border-[#D6D6D6]">
+                  <img src={location} alt="location" />
                 </div>
-
-                <div className="flex items-center w-[322px] h-[60px] relative rounded-[50px] border border-solid border-[#D6D6D6]">
-                  <div className="px-[15px] pr-[15px] border-l border-solid border-[#D6D6D6]">
-                    <img src={location} alt="location" />
-                  </div>
-                  <div className="relative w-full flex px-[10px]">
-                    <label
-                      htmlFor="province"
-                      className="absolute -top-[32px] -right-[16px] px-[10px] bg-white"
-                    >
-                      شهر
-                    </label>
-                    <select
-                      className="w-full"
-                      {...formik.getFieldProps("city")}
-                    >
-                      {provinceRelatedCities.map((city) => (
-                        <option value={city.id} key={city.id}>
-                          {city.name}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
+                <div className="relative w-full flex px-[10px]">
+                  <label
+                    htmlFor="province"
+                    className="absolute -top-[32px] -right-[16px] px-[10px] bg-white"
+                  >
+                    استان
+                  </label>
+                  <select
+                    {...formik.getFieldProps("province")}
+                    // value={formik.getFieldProps("province").value}
+                    className="w-full"
+                  >
+                    {provinces.map((province) => (
+                      <option value={province.id} key={province.id}>
+                        {province.name}
+                      </option>
+                    ))}
+                  </select>
+                  {formik.errors.province ? (
+                    <div className="text-rose-700 text-[14px]">
+                      {formik.errors.province}
+                    </div>
+                  ) : null}
                 </div>
               </div>
-              <TextInput
-                label={"آدرس"}
-                icon={<img src={map} alt="location" />}
-                {...formik.getFieldProps("address")}
-              />
 
-              <div className="relative w-[660px] gap-[16px] flex">
-                <div className="flex items-center w-[322px] h-[60px] relative rounded-[50px] border border-solid border-[#D6D6D6]">
-                  <TextInput
-                    label={"طول جغرافیایی"}
-                    icon={<img src={location} alt="location" />}
-                    {...formik.getFieldProps("lan")}
-                  />
+              <div className="flex items-center w-[322px] h-[60px] relative rounded-[50px] border border-solid border-[#D6D6D6]">
+                <div className="px-[15px] pr-[15px] border-l border-solid border-[#D6D6D6]">
+                  <img src={location} alt="location" />
                 </div>
-
-                <div className="flex items-center w-[322px] h-[60px] relative rounded-[50px] border border-solid border-[#D6D6D6]">
-                  <TextInput
-                    label={"عرض جغرافیایی"}
-                    icon={<img src={location} alt="location" />}
-                    {...formik.getFieldProps("lat")}
-                  />
+                <div className="relative w-full flex px-[10px]">
+                  <label
+                    htmlFor="province"
+                    className="absolute -top-[32px] -right-[16px] px-[10px] bg-white"
+                  >
+                    شهر
+                  </label>
+                  <select className="w-full" {...formik.getFieldProps("city")}>
+                    {provinceRelatedCities.map((city) => (
+                      <option value={city.id} key={city.id}>
+                        {city.name}
+                      </option>
+                    ))}
+                  </select>
                 </div>
               </div>
             </div>
+            <TextInput
+              label={"آدرس"}
+              icon={<img src={map} alt="location" />}
+              {...formik.getFieldProps("address")}
+            />
 
-            <div className="mt-6">
-              <a
-                href="#"
-                className="text-[#388AEA]"
-                onClick={() => setIsShowModal((prev) => !prev)}
-              >
-                انتخاب طول و عرض جغرافیایی از روی نقشه
-              </a>
+            <div className="relative w-[660px] gap-[16px] flex">
+              <div className="flex items-center w-[322px] h-[60px] relative rounded-[50px] border border-solid border-[#D6D6D6]">
+                <TextInput
+                  label={"طول جغرافیایی"}
+                  icon={<img src={location} alt="location" />}
+                  {...formik.getFieldProps("lan")}
+                />
+              </div>
+
+              <div className="flex items-center w-[322px] h-[60px] relative rounded-[50px] border border-solid border-[#D6D6D6]">
+                <TextInput
+                  label={"عرض جغرافیایی"}
+                  icon={<img src={location} alt="location" />}
+                  {...formik.getFieldProps("lat")}
+                />
+              </div>
             </div>
           </div>
-          <div className="flex flex-row-reverse items-center h-[11vh] justify-between">
-            <a to="#" className="no-underline">
-              <button type="submit" className="button">
-                ثبت نام <BsArrowLeft />
-              </button>
-            </a>
+
+          <div className="mt-6">
             <a
-              onClick={() => navigate(-1)}
               href="#"
-              className="text-[#388AEA] no-underline"
+              className="text-[#388AEA]"
+              onClick={() => setIsShowModal((prev) => !prev)}
             >
-              مرحله قبل
+              انتخاب طول و عرض جغرافیایی از روی نقشه
             </a>
           </div>
         </div>
-   
+        <div className="flex flex-row-reverse items-center h-[11vh] justify-between">
+          <a to="#" className="no-underline">
+            <button type="submit" className="button">
+              ثبت نام <BsArrowLeft />
+            </button>
+          </a>
+          <a
+            onClick={() => navigate(-1)}
+            href="#"
+            className="text-[#388AEA] no-underline"
+          >
+            مرحله قبل
+          </a>
+        </div>
+      </div>
     </>
   );
 };
